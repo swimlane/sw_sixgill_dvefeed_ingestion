9a2d12cf6c3c7cad312c1f60cb4a70b7c2015b31
AgFi <agustinfirpo90@gmail.com>
2021-12-08 18:59:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup
 from json import load
-from six import iteritems
 
-def convert(input):
-    if isinstance(input, dict):
-        return {convert(k): convert(v) for k, v in iteritems(input)}
-    elif isinstance(input, list):
-        return [convert(element) for element in input]
-    elif isinstance(input, bytes):
-        return input.decode()
+def convert(_input):
+    if isinstance(_input, dict):
+        return {convert(k): convert(v) for k, v in _input.items()}
+    elif isinstance(_input, list):
+        return [convert(element) for element in _input]
+    elif isinstance(_input, bytes):
+        return _input.decode()
     else:
-        return input
+        return _input
 
 with open("package.json") as config_file:
     config = load(config_file, object_hook=convert)
     config.pop("version_hash")
     setup(**config)
-
